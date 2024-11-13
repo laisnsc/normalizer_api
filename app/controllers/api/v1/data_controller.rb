b27f700266f3ed042b9ba3f6aeebe6f0052fd459
Laís <lais.sdsn@gmail.com>
2024-11-13 18:52:10 -0300

    Added method for data normalization in service, added data controller for processing data

--- app/controllers/api/v1/data_controller.rb
+++ app/controllers/api/v1/data_controller.rb
@@ -1,11 +1,19 @@
 module Api::V1
   class DataController < ApplicationController
+    before_action :data_params
     def process_data
-      file_data = NormalizerService.new(params[:file])
+      file_data = NormalizerService.new(params[:file], params[:order_ids],
+                                        params[:start_date], params[:end_date])
       render json: file_data.process_data, status: :ok
 
     rescue StandardError => e
       render json: { error: e.message }, status: :internal_server_error
+    end
+
+    private
+
+    def data_params
+      params.permit(:file, :order_ids, :start_date, :end_date)
     end
   end
 end
--- app/services/normalizer_service.rb
+++ app/services/normalizer_service.rb
@@ -1,14 +1,19 @@
 class NormalizerService
 
-  def initialize(file)
+  def initialize(file, order_ids=nil, start_date=nil, end_date=nil)
     raise ArgumentError, "File does not exist" if file.blank?
+
+    @order_ids = order_ids
+    @start_date = start_date ? DateTime.parse(start_date) : ""
+    @end_date = end_date ? DateTime.parse(end_date) : ""
     @data = File.readlines(file).map(&:chomp)
   end
 
   def process_data
     normalized_data = normalize_data(@data)
-    persist_data(normalized_data)
-    generate_response(params)
+    # persist_data(normalized_data)
+    filters = build_data_filters(normalized_data['order_ids'])
+    generate_response(filters)
   end
 
   private
@@ -23,13 +28,14 @@ class NormalizerService
       product_id = row[65...75].to_i
       product_name = "prod_#{product_id}"
       value = row[75...87].to_f
-      date = row[87...95]
-
+      date = DateTime.parse(row[87...95])
       user_data[user_id] = { user_id: user_id, name: name }
       user_orders_data << { user_id: user_id, order_id: order_id, date: date, product_id: product_id,
                             value: value, name: product_name }
     end
-    {'users' => user_data.values, 'orders' => user_orders_data }
+
+    file_order_values = user_orders_data.map { |uod| uod[:order_id] }.uniq.sort
+    {'users' => user_data.values, 'orders' => user_orders_data, 'order_ids' => file_order_values }
   end
 
   def persist_data(data)
@@ -39,7 +45,7 @@ class NormalizerService
     find_or_create_object(data['orders'], 'order_product',:order_id, :product_id, :value)
   end
 
-  def find_or_create_object_test(objects, model, *params)
+  def find_or_create_object(objects, model, *params)
     model_class = Object.const_get("#{model.classify}")
     objects.each do |object|
       attributes = params.map { |param| [param, object[param]] }.to_h
@@ -49,17 +55,38 @@ class NormalizerService
     end
   end
 
+  def build_data_filters(file_order_ids)
+    { order_ids: @order_ids || file_order_ids }.merge(build_date_range_filter)
+  end
+
+  def build_date_range_filter
+    return {} unless @start_date.present? && @end_date.present?
+    {
+      date_range: {
+        start_date: @start_date,
+        end_date: @end_date
+      }
+    }
+  end
+
   def generate_response(params)
-    User.with_orders(params).map do |user|
+    users = User.with_orders(params[:order_ids])
+    if params[:date_range].present?
+      users = users.with_orders_by_date(params[:date_range][:start_date], params[:date_range][:end_date])
+    end
+    users.map do |user|
         {
           user_id: user.id,
           name: user.name,
-          orders: get_order_data(user.orders)
+          orders: get_order_data(user.orders.filtered(params[:order_ids]), params[:date_range])
         }
     end
   end
 
-  def get_order_data(orders)
+  def get_order_data(orders, dates)
+    if dates.present?
+      orders = orders.with_date_range(dates[:start_date], dates[:end_date])
+    end
     orders.map do |order|
       {
         order_id: order.id,
